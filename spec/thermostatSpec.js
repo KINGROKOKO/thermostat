25cd59e5e0790b54213e51484ffea955a39d8e83
Thomas Ikimi <themerc01@gmail.com>
2017-09-28 13:24:53 +0100

    up, down and min temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe('Thermostat', function(){
   var thermostat;
 
@@ -8,9 +10,42 @@ describe('Thermostat', function(){
   describe('Check temperature', function(){
 
     it('starts at 20 degrees', function(){
-    expect(thermostat.temperature).toEqual(20)
+    expect(thermostat.getCurrentTemperature()).toEqual(20)
 
     });
   });
+
+  describe('Increase temperature', function(){
+
+    it('with UP by 10 degrees', function(){
+    thermostat.up()
+    expect(thermostat.getCurrentTemperature()).toEqual(21)
+
+    });
+  });
+
+describe('Decrease temperature', function(){
+
+    it('with DOWN by 10 degrees', function(){
+    thermostat.down()
+    expect(thermostat.getCurrentTemperature()).toEqual(19)
+
+    });
+  });
+
+  describe('Do not decrease temperature', function(){
+
+      it('with DOWN if less than 11 degrees', function(){
+
+      var i = 1
+      
+      while(i<11){
+      thermostat.down()
+      i++;
+      }
+      expect(thermostat.down()).toEqual('MIN TEMPERATURE REACHED')
+
+      });
+    });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,20 @@
 var Thermostat = function(){
   this.temperature = 20;
-}
+  this.minTemperature = 10;
+};
+
+Thermostat.prototype.getCurrentTemperature = function(){
+  return this.temperature;
+};
+
+Thermostat.prototype.up = function(){
+  this.temperature += 1;
+};
+
+Thermostat.prototype.down = function(){
+  if(this.temperature > 10){
+  this.temperature -= 1}
+  else {
+  return 'MIN TEMPERATURE REACHED'
+  };
+};
